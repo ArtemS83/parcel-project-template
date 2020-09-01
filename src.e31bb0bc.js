1eafbae2ff47cf58189d4c9e0e9db61fe90b1f61
Atrem Sergieiev <a_serg83@ukr.net>
2020-09-01 21:54:54 +0200

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64501" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57604" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
